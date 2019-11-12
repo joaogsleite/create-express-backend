c1477a756841154a3d28e93fabf3d286b820421f
joaogsleite <joao.leite@isobar.com>
2019-11-12 14:06:46 +0000

    packages upgrade

--- src/generators/default/index.js
+++ src/generators/default/index.js
@@ -2,12 +2,11 @@
 const { scaffold } = require(`${__root}/utils/template`)
 
 module.exports.questions = [
-  { type: 'confirm', name: 'confirmed', message: 'Confirm?', default: false },
   { type: 'input', name: 'aValue', message: 'Variable a value?', default: 1 },
   { type: 'input', name: 'bValue', message: 'Variable b value?', default: 'example' },
 ]
 
-module.exports.run = function ({ aValue, bValue }) {
+module.exports.run = function ({ confirmed, aValue, bValue }) {
   console.log('Creating package.json...')
   console.log('Installing node modules...')
   console.log('Generating project file structure...')
